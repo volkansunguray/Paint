90d284f56ceabe21979f6af592b8cfa6a73b8b80
Fehmi Volkan Sunguray <volkan@sunguray.com>
2022-06-02 14:36:15 +0300

    Text box eklendi.

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -29,11 +29,16 @@
         private void InitializeComponent()
         {
             this.btnrenksec = new System.Windows.Forms.Button();
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnrenksec
             // 
-            this.btnrenksec.Location = new System.Drawing.Point(636, 12);
+            this.btnrenksec.Location = new System.Drawing.Point(12, 130);
             this.btnrenksec.Name = "btnrenksec";
             this.btnrenksec.Size = new System.Drawing.Size(152, 32);
             this.btnrenksec.TabIndex = 0;
@@ -41,25 +46,97 @@
             this.btnrenksec.UseVisualStyleBackColor = true;
             this.btnrenksec.Click += new System.EventHandler(this.btnrenksec_Click);
             // 
+            // btnYaz
+            // 
+            this.btnYaz.Location = new System.Drawing.Point(12, 92);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(152, 32);
+            this.btnYaz.TabIndex = 1;
+            this.btnYaz.Text = "Text\'i Yazdır";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Arial",
+            "Helvetica",
+            "Times",
+            "Courier",
+            "Verdana",
+            "Courier"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "";
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Text\'i Buraya Yazınız";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "12",
+            "14",
+            "16",
+            "24",
+            "30",
+            "40"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 65);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(152, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Paleti Temizle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1264, 729);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnYaz);
             this.Controls.Add(this.btnrenksec);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnrenksec;
+        private System.Windows.Forms.Button btnYaz;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -23,7 +23,8 @@ namespace Paint
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            comboBox1.SelectedText = "Arial";
+            comboBox2.SelectedText = "12";
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
@@ -54,10 +55,27 @@ namespace Paint
             ciz = false;
         }
 
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            
+            string yazitipi = comboBox1.Text;
+            Graphics g = this.CreateGraphics();
+            Font myFont = new Font(yazitipi, Convert.ToInt32(comboBox2.Text));
+            Brush myBrush = new SolidBrush(renksec.Color);
+            g.DrawString(textBox1.Text, myFont, myBrush, 175, 20);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+        }
 
         private void btnrenksec_Click(object sender, EventArgs e)
         {
             renksec.ShowDialog();
+
         }
 
 
